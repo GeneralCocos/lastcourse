d44a4de0eb966969ac36a456c3f697ac9e52c3b9
GeneralCocos <vl@d-sotskov.ru>
2025-02-20 12:17:53 +0300

    LR3 EX1

--- src/components/App/app.tsx
+++ src/components/App/app.tsx
@@ -1,4 +1,13 @@
 import MainPage from "../../pages/main-page/main-page"
+import FavoritesPage from "../../pages/favorites-page/favorites-page";
+import LoginPage from "../../pages/login-page/login-page";
+import OfferPage from "../../pages/offer-page/offer-page";
+import NotFoundPage from "../../pages/not-found-page/not-found-page";
+import { BrowserRouter } from "react-router-dom";
+import { Route } from "react-router-dom";
+import { Routes } from "react-router-dom";
+import { AppRoute } from "../../const";
+
 
 type AppMainPageProps = {
     rentalOffersCount: number;
@@ -6,7 +15,31 @@ type AppMainPageProps = {
 
 function App({rentalOffersCount} : AppMainPageProps): JSX.Element {
     return (
-        <MainPage rentalOffersCount={rentalOffersCount}/>
+        <BrowserRouter>
+        <Routes>
+            <Route
+            path={AppRoute.Main}
+            element={<MainPage rentalOffersCount={rentalOffersCount}/>}/>
+
+            <Route
+            path={AppRoute.Login}
+            element={<LoginPage/>}/>
+
+            <Route
+            path={AppRoute.Favorites}
+            element={<FavoritesPage/>}/>
+
+            <Route
+            path={AppRoute.Offer}
+            element={<OfferPage/>}/>
+
+            <Route
+            path='*'
+            element={<NotFoundPage/>}/>
+
+        </Routes>
+        
+        </BrowserRouter>
     );
 }
 
--- src/const.ts
+++ src/const.ts
@@ -2,6 +2,13 @@ const Setting ={
     rentOffersCount: 312,
 } as const;
 
+const AppRoute = {
+    Main : '/',
+    Login : '/login',
+    Favorites : '/favorites',
+    Offer : '/offer/:id',
+} as const;
 
 
 export  {Setting};
+export {AppRoute};
